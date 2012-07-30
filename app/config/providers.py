4ec7fdbfa21d6902859c3de583ce3f3a1c4ea953
unknown <Sam@AD-ULTRAFOUR.hq.momentum.io>
2012-07-29 21:30:02 -0700

    Installed external data provider libraries

---
+++ app/config/providers.py
@@ -0,0 +1,22 @@
+
+config['fatcatmap.data.providers'] = {
+
+	'installed': ['SunlightFoundation', 'Freebase']
+
+}
+
+
+config['fatcatmap.data.providers.Freebase'] = {
+
+	'enabled': False,
+	'apikey': ''
+
+}
+
+
+config['fatcatmap.data.providers.SunlightFoundation'] = {
+
+	'enabled': False,
+	'apikey': '30de49bc54024e689ad0859a9414bf79'
+
+}
